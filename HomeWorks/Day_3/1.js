03095b665bc2d7160353eb243f13a2dd1fff407e
Ivan Tereshchenko <geminimzk@gmail.com>
2023-04-24 11:26:10 +0400

    added homework_Day_3

---
+++ HomeWorks/Day_3/1.js
@@ -0,0 +1,4 @@
+let celsius = parseFloat(prompt("Введите температуру в градусах Цельсия:"));
+let fahrenheit = (9 / 5) * celsius + 32;
+fahrenheit = Math.round(fahrenheit * 10) / 10;
+alert("Цельсий: " + celsius + ", Фаренгейт: " + fahrenheit);
